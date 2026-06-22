29c5b575a2541d557160a5946a5ee1fb32e9d7b4
Julian Pasos <127365421+EJPasos@users.noreply.github.com>
2026-06-21 21:19:26 -0700

    Cambios en player
    Se agrego codigo para animar correctamente el movimiento del jugador

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -330.0
-
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,9 +17,12 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left", "Right")
+	sprite.play("Walk")
 	if direction:
 		velocity.x = direction * SPEED
+		sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		sprite.play("IDLE")
 
 	move_and_slide()
